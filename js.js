72d0456fa7e171fadeb5fe8ed6cf6817ceb10bcc
iCarly <crestcere@outlook.com>
2023-04-01 18:53:18 +0300

    Improvements

--- js.js
+++ js.js
@@ -1,24 +1,65 @@
+
 // Make gameboard object
 // And make gameboard array inside of object
 // Flow game time object
+
+const body = document.querySelector(".body");
 const gameBoard = (() => {
-    const board = [];
-    const flow = 0;
-    const checkBoard = () => {};
-    const addX = () => {
-        checkBoard();
-        board.push("X");
-    }
-    const addO = () => {
-        checkBoard();
-        board.push("O");
+    // IIFE
+    const board = ["", "", "", "", "", "", "", "", ""];
+
+    let flow = 0;
+    const checkBoard = () => {
+        // const checkAlready = () => {
+        //     if (board[count] === "X" || "O") {
+        //         console.log("Already picked!", board[count]);
+        //         return false;
+        //     } else {
+        //         return true;
+        //     }
+        // }
+        // if ((flow > 8) || (checkAlready() == true)) {
+        if ((flow > 8)) {
+            console.log("Flow Ended!");
+            return false;
+        } else {
+            return true;
+        }
+    };
+    const fill = (count = 0) => {
+        if (checkBoard()) {
+            if (flow % 2 == 0) {
+                board[count] = "X";
+                flow++;
+            } else {
+                board[count] = "O";
+                flow++;
+            }
+        }
+        printBoard();
     }
     const printBoard = () => {
-        return board;
+        body.innerHTML = "";
+        let count = 0;
+        let store = [];
+        for (let i = 0; i < board.length; i = i + 3) {
+             store.push(
+                `
+                    <div style="display: flex; flex-direction: row">
+                        <div class="${count}" onclick="gameBoard.fill(${count})" style="border: 1px solid black; margin: 2px; height: 20px; width: 20px;">${board[i]}</div>
+                        <div class="${count+1}" onclick="gameBoard.fill(${count+1})" style="border: 1px solid black; margin: 2px; height: 20px; width: 20px;">${board[i+1]}</div>
+                        <div class="${count+2}" onclick="gameBoard.fill(${count+2})" style="border: 1px solid black; margin: 2px; height: 20px; width: 20px;">${board[i+2]}</div>
+                    </div>    
+                `);
+             count += 3;
+        }
+        let a;
+        a = store.join(" ");
+        body.innerHTML = a;
+        return {board};
     }
-    return {addX, addO, printBoard}
+    return {fill, printBoard}
 })();
-
 // Object for players
 
 const player = () => {
@@ -46,3 +87,7 @@ player1.addScore();
 player2.removeScore();
 
 console.log("player 2: ", player2.printScore());
+console.log("board: ", gameBoard.printBoard());
+gameBoard.printBoard();
+
+
